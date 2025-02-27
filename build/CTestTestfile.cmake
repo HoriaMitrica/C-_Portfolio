85700c2d607af5118f99025fc7288fdc63f45088
Horia Mitrica <horia.mitrica@gmail.com>
2025-02-27 10:30:51 +0200

    Remove ignored files from Git tracking

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mitri/GitProjects/C-_Portfolio
-# Build directory: /home/mitri/GitProjects/C-_Portfolio/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(BasicTasksTests "/home/mitri/GitProjects/C-_Portfolio/build/tests")
-set_tests_properties(BasicTasksTests PROPERTIES  _BACKTRACE_TRIPLES "/home/mitri/GitProjects/C-_Portfolio/CMakeLists.txt;56;add_test;/home/mitri/GitProjects/C-_Portfolio/CMakeLists.txt;0;")
